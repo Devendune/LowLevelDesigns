39135117fa4bca97648fe29f1a098ae7dde343f8
Devendune <devendunegi06@gmail.com>
2025-09-21 13:46:19 +0530

    TicTacTo Logic

--- src/main/java/org/TicTacToe/Player.java
+++ src/main/java/org/TicTacToe/Player.java
@@ -5,6 +5,11 @@ public class Player
     String name;
     PlayingPiece playingPiece;
 
+    public Player(String name, PlayingPiece playingPiece) {
+        this.name = name;
+        this.playingPiece = playingPiece;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/org/TicTacToe/TicTacToeGame.java
+++ src/main/java/org/TicTacToe/TicTacToeGame.java
@@ -6,6 +6,19 @@ import java.util.Timer;
 
 public class TicTacToeGame
 {
+    Deque<Player>deque;
+    Board gameBoard;
+
+
+
+    public void initializeGame()
+    {
+        Player player1=new Player("Devendu",new PlayerX());
+        Player player2=new Player("Tushar",new PlayerO());
+
+        gameBoard=new Board(3);
+
+    }
 
 
 }
